41ca8780fe7c36acd0e2e57e41508d30e4a4fc2b
Joe Downard <joehdownard@gmail.com>
2020-11-30 14:42:33 +0000

    shader

---
+++ BunnyShader.js
@@ -0,0 +1,29 @@
+var BunnyShader = {
+
+	uniforms: {},
+
+	vertexShader: [
+
+        "varying vec4 vertex;",
+
+		"void main() {",
+
+        "	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+        "   vertex = vec4(gl_Position);",
+
+		"}"
+
+	].join( "\n" ),
+
+	fragmentShader: [
+        "varying vec4 vertex;",
+
+		"void main() {",
+
+		"	gl_FragColor = vertex;",
+
+		"}"
+
+	].join( "\n" )
+
+};
