4a552742acbe721061b7f56fbcc7e40abe844e08
Joe Downard <joehdownard@gmail.com>
2020-12-10 17:32:08 +0000

    shader upgrade and made code look a bit better

--- BunnyShader.js
+++ BunnyShader.js
@@ -1,29 +1,20 @@
 var BunnyShader = {
-
-	uniforms: {},
-
 	vertexShader: [
-
-		"varying vec4 vertex;",
-
+		"varying vec4 colour;",
 		"void main() {",
-
 		"	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-		"   vertex = vec4(gl_Position);",
-
+		"   colour = vec4(gl_Position);",
 		"}"
 
 	].join("\n"),
 
 	fragmentShader: [
-		"varying vec4 vertex;",
-
+		"varying vec4 colour;",
+		"uniform float time_factor;",
 		"void main() {",
-
-		"	gl_FragColor = vertex;",
-
+		"	vec4 resultant_colour = colour * time_factor;",
+		"	gl_FragColor = resultant_colour;",
 		"}"
 
 	].join("\n")
-
 };	
